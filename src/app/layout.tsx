651f06394cee5d5b6fccca5fcee2f78693ae5da5
thiago morgado <thiago_morgado@hotmail.com>
2025-01-29 00:00:04 -0300

    atualizando metaag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,24 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "CasaInteligente - Transforme Sua Casa com Alexa",
   description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
+  keywords: "Casa Inteligente, Alexa, Automação, Tecnologia, Conforto, Conveniência",
+  openGraph: {
+    title: "CasaInteligente - Transforme Sua Casa com Alexa",
+    description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
+    url: "https://www.casainteligente.com", // URL do seu site
+    images: [
+      "https://www.casainteligente.com/imagem-compartilhamento.jpg", // URL da imagem para compartilhamento
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CasaInteligente - Transforme Sua Casa com Alexa",
+    description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
+    images: [
+      "https://www.casainteligente.com/imagem-compartilhamento.jpg", // URL da imagem para compartilhamento
+    ],
+  },
 }
 
 export default function RootLayout({
@@ -19,9 +37,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <body className={`${inter.className} antialiased`} suppressHydrationWarning={true}>
-       
+        <Header />
         {children}
-       
+        <Footer />
       </body>
     </html>
   )
