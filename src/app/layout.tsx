5e19d7b7ae2bf42ed5e1c054b8838220e5e93794
thiago morgado <thiago_morgado@hotmail.com>
2025-01-29 00:04:46 -0300

    atualizar imagen de compartilhamento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
     url: "https://www.casainteligente.com", // URL do seu site
     images: [
-      "https://www.casainteligente.com/imagem-compartilhamento.jpg", // URL da imagem para compartilhamento
+      "/Logo.jpg", // URL da imagem para compartilhamento
     ],
     type: "website",
   },
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     title: "CasaInteligente - Transforme Sua Casa com Alexa",
     description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
     images: [
-      "https://www.casainteligente.com/imagem-compartilhamento.jpg", // URL da imagem para compartilhamento
+      "/Logo.jpg", // URL da imagem para compartilhamento
     ],
   },
 }
