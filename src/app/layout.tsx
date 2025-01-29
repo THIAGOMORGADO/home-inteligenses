4d61611ef91d98eae380144182c7e8585fafbbf2
thiago morgado <thiago_morgado@hotmail.com>
2025-01-29 00:44:13 -0300

    atat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
-export const metadata = {
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
   title: "CasaInteligente - Transforme Sua Casa com Alexa",
   description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
   openGraph: {
@@ -21,5 +29,21 @@ export const metadata = {
     title: "CasaInteligente - Transforme Sua Casa com Alexa",
     description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
     images: ["https://home-inteligenses.vercel.app/Logo.png"],
-  },
 };
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR" className="scroll-smooth">
+      <body className={`${inter.className} antialiased`} suppressHydrationWarning={true}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
