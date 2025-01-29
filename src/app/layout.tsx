a0cd618081d63acac5575e2292b8232041d264df
thiago morgado <thiago_morgado@hotmail.com>
2025-01-29 00:42:23 -0300

    at

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,4 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-
-const inter = Inter({ subsets: ["latin"] })
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "CasaInteligente - Transforme Sua Casa com Alexa",
   description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
   openGraph: {
@@ -15,7 +7,8 @@ export const metadata: Metadata = {
     url: "https://home-inteligenses.vercel.app",
     images: [
       {
-        url: "/Logo.png",
+        url: "https://home-inteligenses.vercel.app/Logo.png",
+        secure_url: "https://home-inteligenses.vercel.app/Logo.png",
         width: 1200,
         height: 630,
         alt: "Casa Inteligente - Logo",
@@ -27,23 +20,6 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "CasaInteligente - Transforme Sua Casa com Alexa",
     description: "Descubra o futuro da vida inteligente com conforto e conveniência controlados por voz.",
-    images: ["/Logo.png"],
+    images: ["https://home-inteligenses.vercel.app/Logo.png"],
   },
 };
-
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="pt-BR" className="scroll-smooth">
-      <body className={`${inter.className} antialiased`} suppressHydrationWarning={true}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
-}
